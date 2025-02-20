f9774bc2d484655cfe88d79cd186497cda7a8e24
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-02-20 11:54:38 -0500

    create a route file, create schema

---
+++ modals/LogSchema.mjs
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+//create the schema for the logs modal
+//log id - auto generated
+// Location visited  string
+//Country located in (incase i want to integreate countries api to display countries visited by user and yet to visit) string
+//date (optional) for now
+//rating - number
+//isFavorite - boolean (optional, false by default)
+//additional comments - user can describe their overall experiences, things they liked and did not like, things other users can be aware of if they want to visit (user login feature in the future and other users are able to see if different logs from different user. Not implemented in this application)
+//date created - auto generated
+
+const LogSchema = new mongoose.Schema(
+  {
+    log_id: Number,
+    location: { type: String, required: true },
+    country: { type: String, required: true },
+    date_visited: { type: Date, default: "N/A" },
+    rating: { type: Number, required: true },
+    isFavorite: { type: Boolean, default: false },
+    additional_comments: {
+      type: String,
+      default: "Additional details not provided",
+    },
+    log_date: { type: Date, default: Date.now().toLocaleString() },
+  },
+  {
+    versionKey: false,
+  }
+);
+LogSchema.index({ log_id: 1 }, { unique: true });
+
+const Log = mongoose.model("Log", LogSchema);
+
+export default Log;
---
+++ routes/logs.mjs
@@ -0,0 +1,11 @@
+//get routes to get all the logs (/logs) // only display selected details through the front end
+
+// get by id /details/id to get a detail on specific log, display all the information
+
+// post to add a log //on add page /logs/add
+
+// patch to edit the logs // on edit page /logs/edit
+
+//delete by id /logs/:id  // option to delete will be on the landing/homepage
+
+//toogle favorite // remove from favorite available in the favorite page
