1fbc70b8ea718fe9f87bc144c4f9a7a2ea3a5270
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-02-21 15:26:32 -0500

    add the get route to get all the logs

--- routes/logs.mjs
+++ routes/logs.mjs
@@ -9,3 +9,21 @@
 //delete by id /logs/:id  // option to delete will be on the landing/homepage
 
 //toogle favorite // remove from favorite available in the favorite page
+import express from "express";
+import Logs from "../modals/LogSchema.mjs";
+// import mongoose from "mongoose";
+import { check, validationResult } from "express-validator";
+
+//define the router
+const router = express.Router();
+
+//route to get all the logs in the database
+router.get("/logs", async (req, res) => {
+  try {
+    let result = await Logs.find().sort({ log_id: 1 });
+    res.status(200).json({ logs: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Unable to fetch data due to server error" });
+  }
+});
