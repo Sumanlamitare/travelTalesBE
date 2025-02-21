31f786e40d816b2c03c249f8d24f8bcf3f152065
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-02-21 15:23:53 -0500

    add middleware for cors

--- server.mjs
+++ server.mjs
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import db from "./db/conn.mjs";
+import router from "../travelTalesBE/routes/logs.mjs";
+import cors from "cors";
 
 const app = express();
 
@@ -7,12 +10,17 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
+//connect to the database
+db();
+
 //express middleware
 
 app.use(express.json());
+app.use(cors());
 
 //landing page route for testing
 
+app.use("/", router);
 app.use("/", (req, res) => {
   res.json({ Status: "Running" });
 });
