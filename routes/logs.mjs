c76c7edf60e6c6f2f119a090b42279c575e611d5
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-02-21 15:29:25 -0500

    add routes to add logs to the database, use express validator to verify body

--- routes/logs.mjs
+++ routes/logs.mjs
@@ -46,3 +46,72 @@ router.get("/logs/:id", async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+//post route for logs
+router.post(
+  "/logs",
+  [
+    check("location").not().isEmpty().withMessage("Location is required"),
+    check("country").not().isEmpty().withMessage("Country is required"),
+    check("date_visited")
+      .optional()
+      .isISO8601()
+      .withMessage("Please enter a date format"),
+    check("rating")
+      .isInt({ min: 1, max: 10 })
+      .withMessage("Please provide a rating, 1-10"),
+  ],
+  async (req, res) => {
+    // console.log("We are here");
+    const location = req.body.location;
+    const country = req.body.country;
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    let results = await Logs.findOne().sort({ _id: -1 });
+    let postValidate = await Logs.findOne({
+      location: location,
+      country: country,
+    }).collation({ locale: "en", strength: 2 });
+
+    if (!postValidate) {
+      try {
+        if (results) {
+          req.body.log_id = results.log_id + 1;
+        } else {
+          req.body.log_id = 1;
+        }
+        const {
+          log_id,
+          location,
+          country,
+          date_visited,
+          rating,
+          isFavorite,
+          additional_comments,
+        } = req.body;
+
+        const log = new Logs({
+          log_id,
+          location,
+          country,
+          date_visited,
+          rating,
+          isFavorite,
+          additional_comments,
+        });
+
+        await Logs.create(log);
+
+        res.status(200).json({ status: "Added", log: log });
+      } catch (err) {
+        console.error(err);
+        res.status(400).json({ error: "Logs not added due to " + err });
+      }
+    } else {
+      res.status(400).json({
+        error: `Log for  ${req.body.location}, ${req.body.country} already exists.`,
+      });
+    }
+  }
+);
